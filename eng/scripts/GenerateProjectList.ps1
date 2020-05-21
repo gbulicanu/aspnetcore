f9ceb721665ac6a2a5f2b30d714f2d3a711af5a9
Doug Bunting <6431421+dougbu@users.noreply.github.com>
2020-05-20 21:23:49 -0700

    Adjust generation scripts to build using `dotnet msbuild`

--- eng/scripts/GenerateProjectList.ps1
+++ eng/scripts/GenerateProjectList.ps1
@@ -3,8 +3,12 @@ param(
 )
 $ErrorActionPreference = 'stop'
 
+$Configuration = if ($ci) { 'Release' } else { 'Debug' }
 $repoRoot = Resolve-Path "$PSScriptRoot/../.."
+$MsbuildEngine = 'dotnet'
 
 & "$repoRoot\eng\common\msbuild.ps1" -ci:$ci "$repoRoot/eng/CodeGen.proj" `
+    -configuration $Configuration `
+    -msbuildEngine $MsbuildEngine `
     /t:GenerateProjectList `
     /bl:artifacts/log/genprojlist.binlog
--- eng/scripts/GenerateReferenceAssemblies.ps1
+++ eng/scripts/GenerateReferenceAssemblies.ps1
@@ -5,6 +5,12 @@ $ErrorActionPreference = 'stop'
 
 $repoRoot = Resolve-Path "$PSScriptRoot/../.."
 
+$Configuration = if ($ci) { 'Release' } else { 'Debug' }
+& "$repoRoot\build.ps1" -ci:$ci -buildNative -configuration $Configuration
+
+$MsbuildEngine = 'dotnet'
 & "$repoRoot\eng\common\msbuild.ps1" -ci:$ci "$repoRoot/eng/CodeGen.proj" `
+    -configuration $Configuration `
+    -msbuildEngine $MsbuildEngine `
     /t:GenerateReferenceSources `
     /bl:artifacts/log/genrefassemblies.binlog
